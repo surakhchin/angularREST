f3182c778ab84c5b07e8553c1d2aa61ee50ec67a
Serge <urakhchin@gmail.com>
2017-05-21 06:11:04 -0700

    Setting up UI, currently working on DELETE button, it works on selecting id value but i need it to delete object in the array with that value...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,77 +8,73 @@ angular.module('confusionApp', ['ui.router','ngResource'])
             .state('app', {
                 url:'/',
                 views: {
-                    'header': {
-                        templateUrl : 'views/header.html',
-                    },
+
                     'content': {
-                        templateUrl : 'views/home.html',
-                        controller  : 'IndexController'
+                        templateUrl : 'views/header.html',
+                        controller  : 'JumboController'
                     },
-                    'footer': {
-                        templateUrl : 'views/footer.html',
-                    }
+
                 }
 
-            })
-        
-            // route for the aboutus page
-            .state('app.aboutus', {
-                url:'aboutus',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/aboutus.html',
-                        controller  : 'AboutController'                  
-                    }
-                }
-            })
-        
-            // route for the contactus page
-            .state('app.contactus', {
-                url:'contactus',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/contactus.html',
-                        controller  : 'ContactController'                  
-                    }
-                }
-            })
-
-            // route for the menu page
-            .state('app.menu', {
-                url: 'menu',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/menu.html',
-                        controller  : 'MenuController'
-                    }
-                }
-            })
-
-
-                // route for the menu#menu id in menu page
-            .state('app.menuid', {
-                url: 'menu#menu',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/menu.html',
-                        controller  : 'MenuController'
-                    }
-                }
-            })
-
-
-
-            // route for the dishdetail page
-            .state('app.dishdetails', {
-                url: 'menu/:id',
-                views: {
-                    'content@': {
-                        templateUrl : 'views/dishdetail.html',
-                        controller  : 'DishDetailController'
-                   }
-                }
             });
+        
+            // // route for the aboutus page
+            // .state('app.aboutus', {
+            //     url:'aboutus',
+            //     views: {
+            //         'content@': {
+            //             templateUrl : 'views/aboutus.html',
+            //             controller  : 'AboutController'
+            //         }
+            //     }
+            // })
+            //
+            // // route for the contactus page
+            // .state('app.contactus', {
+            //     url:'contactus',
+            //     views: {
+            //         'content@': {
+            //             templateUrl : 'views/contactus.html',
+            //             controller  : 'ContactController'
+            //         }
+            //     }
+            // })
+            //
+            // // route for the menu page
+            // .state('app.menu', {
+            //     url: 'menu',
+            //     views: {
+            //         'content@': {
+            //             templateUrl : 'views/menu.html',
+            //             controller  : 'MenuController'
+            //         }
+            //     }
+            // })
+            //
+            //
+            //     // route for the menu#menu id in menu page
+            // .state('app.menuid', {
+            //     url: 'menu#menu',
+            //     views: {
+            //         'content@': {
+            //             templateUrl : 'views/menu.html',
+            //             controller  : 'MenuController'
+            //         }
+            //     }
+            // })
+            //
+            //
+            //
+            // // route for the dishdetail page
+            // .state('app.dishdetails', {
+            //     url: 'menu/:id',
+            //     views: {
+            //         'content@': {
+            //             templateUrl : 'views/dishdetail.html',
+            //             controller  : 'DishDetailController'
+            //        }
+            //     }
+            // });
     
         $urlRouterProvider.otherwise('/');
     }])
